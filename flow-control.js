02b5dcbbc55a71a74d9502cff7002da4fbe3a0ac
Unknown <doprp25@yahoo.com>
2020-01-09 00:58:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function ageChecker(age) {
   if (age > 12 && age < 20)
   return("You are a teenager!")
     else if (age < 13) {
-      return("You area a kid")
+      return("You are a kid")
     }
 }
 
